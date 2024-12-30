179a59008b8452a43a45276945d241871e4a27c5
chaptsand <chaptsand@gmail.com>
2024-12-30 09:51:50 -0300

    dm1q: Switch to python extract_utils Change-Id: Idbd280e769725b326512590abd2fc8d684fab4dd

---
+++ extract-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/sm8550-common',
+    'hardware/qcom-caf/sm8550',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/samsung/sm8550-common',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libsecril-client',
+    ): lib_fixup_vendor_suffix,
+}
+
+module = ExtractUtilsModule(
+    'dm1q',
+    'samsung',
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8550-common', module.vendor)
+    utils.run()
